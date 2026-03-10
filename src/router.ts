0accf33b6ea277732dd1f69d16b4e71d3f314c33
sophiemorales6440 <sophiemorales6440@github.com>
2026-03-10 17:34:38 +0100

    feat: Add categories CRUD

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import categoriesRoutes from "./modules/category/categoryRoutes.js";
 import itemsRoutes from "./modules/items/itemsRoutes.js";
 
 const router = Router();
 
 router.use("/api/items", itemsRoutes);
+router.use("/api/categories", categoriesRoutes);
 
 export default router;
