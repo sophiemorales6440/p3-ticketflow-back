24d9aa183a08d46f7be0d5b8b697832daa55b6b4
Victor dS <victordespirlet@gmail.com>
2026-03-10 17:30:08 +0100

    feat: mise à jour du seed SQL pour les users

--- src/database/seed.sql
+++ src/database/seed.sql
@@ -1,6 +1,14 @@
 INSERT INTO items (title) VALUES
   ('Premier item'),
   ('Deuxième item'),
+  ('test item test'),
   ('Troisième item');
 
 -- data fictive pour dev à mettre dans les tables
+INSERT INTO users (firstname, lastname, email, password)
+VALUES 
+('Tom', 'Doe', 'Tom@example.com', 'azerty'),
+('Marie', 'Dupont', 'marie@example.com', '123456789'),
+('James', 'Cook', 'james@example.com', 'motdepasse'),
+('Victor', 'Doer', 'victor@example.com', 'gitgud'),
+('John', 'Battlefield', 'John@example.com', 'getrekt');
