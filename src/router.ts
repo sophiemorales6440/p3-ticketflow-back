76431dbfc4af5455f404845df3cc0c5df13cae0b
Victor dS <victordespirlet@gmail.com>
2026-03-10 17:30:38 +0100

    feat: mise à jour des routes users et du router principal

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import itemsRoutes from "./modules/items/itemsRoutes.js";
+import usersRoutes from "./modules/users/usersRoutes.js";
 
 const router = Router();
 
 router.use("/api/items", itemsRoutes);
+router.use("/api/users", usersRoutes);
 
 export default router;
