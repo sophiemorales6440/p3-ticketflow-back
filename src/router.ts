4cf719657e26fa3cfdf1eb13bbe02acea7874b61
Victor dS <victordespirlet@gmail.com>
2026-03-11 09:58:43 +0100

    feat: mise à jour des routes users et du router principal

--- src/router.ts
+++ src/router.ts
@@ -1,18 +1,12 @@
 import { Router } from "express";
 import itemsRoutes from "./modules/items/itemsRoutes.js";
-<<<<<<< DB_users_Victor
-import usersRoutes from "./modules/users/usersRoutes.js";
-=======
 import ticketsRoutes from "./modules/tickets/ticketsRoutes.js";
->>>>>>> dev
+import usersRoutes from "./modules/users/usersRoutes.js";
 
 const router = Router();
 
 router.use("/api/items", itemsRoutes);
-<<<<<<< DB_users_Victor
 router.use("/api/users", usersRoutes);
-=======
 router.use("/api/tickets", ticketsRoutes);
->>>>>>> dev
 
 export default router;
