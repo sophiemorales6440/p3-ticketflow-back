c0b88a8e51a2a65f669e2d9cde4551fb5a6dbb77
Karim13WildCodeSchool <kde13013@gmail.com>
2026-03-11 09:54:04 +0100

    fix(router): resolve merge and update routes

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import attachmentsRoutes from "./modules/attachments/attachmentsRoutes.js";
+import categoriesRoutes from "./modules/category/categoryRoutes.js";
 import itemsRoutes from "./modules/items/itemsRoutes.js";
+import ticketsRoutes from "./modules/tickets/ticketsRoutes.js";
 
 const router = Router();
 
 router.use("/api/items", itemsRoutes);
-router.use(attachmentsRoutes);
+router.use("/api/tickets", ticketsRoutes);
+router.use("/api/categories", categoriesRoutes);
+router.use("/api/attachments", attachmentsRoutes);
 
 export default router;
