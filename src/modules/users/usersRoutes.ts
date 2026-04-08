51056e8dec68515826bef0eecd54e70eb4c45f9e
Victor dS <victordespirlet@gmail.com>
2026-04-08 18:03:43 +0200

    feat: ajout du back end pour afficher les tickets par tech

--- src/modules/users/usersRoutes.ts
+++ src/modules/users/usersRoutes.ts
@@ -5,11 +5,13 @@ import {
 	getAll,
 	getById,
 	update,
+	getTechnicianTickets,
 } from "./usersControllers.js";
 
 const router = Router();
 
 router.get("/", getAll);
+router.get("/:user/tickets", getTechnicianTickets);
 router.get("/:user", getById);
 router.post("/", create);
 router.put("/:user", update);
