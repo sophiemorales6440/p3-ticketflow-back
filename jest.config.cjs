0a66da96e09adc6d2b51877d921e9a9da6d2de75
Marine_O <marine.opsommer@gmail.com>
2026-04-20 15:56:02 +0200

    chore(tests): ajout du mock multer pour les tests jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
 	testEnvironment: "node",
 	moduleNameMapper: {
 		"^(\\.{1,2}/.+)\\.js$": "$1",
+		"^multer$": "<rootDir>/src/__mocks__/multer.ts",
 	},
 	testMatch: ["**/src/**/__tests__/**/*.test.ts"],
 	transform: {
